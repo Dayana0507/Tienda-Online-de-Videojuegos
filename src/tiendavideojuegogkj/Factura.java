52d08cc9e5dac54fc18ceb4747b733dc7bfd7354
Diarmuid6 <90366174+Diarmuid6@users.noreply.github.com>
2021-09-17 23:07:12 -0500

    se creo la clase Factura,se creo 10 paneles

---
+++ src/tiendavideojuegogkj/Factura.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiendavideojuegogkj;
+
+/**
+ *
+ * @author compu
+ */
+public class Factura {
+    
+    private double precioTotal=0;
+    private String cedula;
+    private String nombreComprador;
+    private String direccion;
+    private String tarjetaUsada;
+    private String tipoTarjeta;
+    
+    public double getPrecioTotal() {
+        return precioTotal;
+    }
+
+    public void setPrecioTotal(double precioTotal) {
+        this.precioTotal = precioTotal;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombreComprador() {
+        return nombreComprador;
+    }
+
+    public void setNombreComprador(String nombreComprador) {
+        this.nombreComprador = nombreComprador;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTarjetaUsada() {
+        return tarjetaUsada;
+    }
+
+    public void setTarjetaUsada(String tarjetaUsada) {
+        this.tarjetaUsada = tarjetaUsada;
+    }
+
+    public String getTipoTarjeta() {
+        return tipoTarjeta;
+    }
+
+    public void setTipoTarjeta(String tipoTarjeta) {
+        this.tipoTarjeta = tipoTarjeta;
+    }
+    
+    
+
+    
+}
