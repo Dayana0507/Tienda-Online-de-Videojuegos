bf8039f7f03013157abbc8da7b788d80264d8688
Dayana0507 <karen.guana01@epn.edu.ec>
2021-09-17 23:50:24 -0500

    Se inserto imagenes a 10 videojuegos

--- src/tiendavideojuegogkj/PnlAlienInsolation.java
+++ src/tiendavideojuegogkj/PnlAlienInsolation.java
@@ -58,6 +58,7 @@ public class PnlAlienInsolation extends javax.swing.JPanel {
 
         jLabel5.setText("Compra realizada con exito");
 
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/tiendavideojuegogkj/AlienInsolation.png"))); // NOI18N
         jLabel2.setText("jLabel2");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
--- src/tiendavideojuegogkj/PnlAsteriod.java
+++ src/tiendavideojuegogkj/PnlAsteriod.java
@@ -57,6 +57,7 @@ public class PnlAsteriod extends javax.swing.JPanel {
 
         jLabel5.setText("Compra realizada con exito");
 
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/tiendavideojuegogkj/Asteroids.png"))); // NOI18N
         jLabel2.setText("jLabel2");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
